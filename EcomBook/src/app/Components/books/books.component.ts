4993c2ffa7fbd2b31cad45ee1928075728b43017
Chhandamayee Samal <34711450+Chhandamayee@users.noreply.github.com>
2023-06-08 19:55:53 +0530

    Book Data Fetch Done

--- EcomBook/src/app/Components/books/books.component.ts
+++ EcomBook/src/app/Components/books/books.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { BooksService } from 'src/app/Services/books.service';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  apiData: any;
+  constructor(private http: BooksService) {
+    
+   }
+  ngOnInit(): void {
+    this.http.getBooks().subscribe(response=>{
+      this.apiData = response;
+    })
+      
+  }
 
 }
